92a17d30b444eb00d5cdec32642e0044d0c3aaf2
mathiusaldi <matheus.geraldi@sirclo.com>
2025-01-25 18:39:51 +0700

    initial project hagioskids

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<x-head></x-head>
+<body>
+    <!--
+  This example requires updating your template:
+
+  ```
+  <html class="h-full bg-gray-100">
+  <body class="h-full">
+  ```
+-->
+<div class="min-h-full">
+  <x-navbar></x-navbar>
+  @if(isset($title) && $title)
+    <x-header>{{ $title }}</x-header>
+  @endif
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      {{ $slot }}
+    </div>
+  </main>
+</div>
+
+</body>
+</html>
